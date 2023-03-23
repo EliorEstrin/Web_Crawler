df92cfbebdea6e31af9266e30f1188f578aeab83
elior <elior1001@gmail.com>
2023-03-24 01:12:29 +0200

    added test to check if a file can be created

--- test_unitest.py
+++ test_unitest.py
@@ -1,17 +1,27 @@
 import pytest
 import os
 
+# from url_class import Url
+from url_class import Url
 
-URL='http://localhost.com/'
+# URL='http://localhost.com/'
 
-
-def test_can_create_file():
+# easy test
+def test_can_save_single_html_in_folder():
     """
-    expects a '<depth>/URL'  
+    expects a '<depth>/URL.html' folder created with valid html inside.
+    this is an easy test means it only checks for depth=0 which is the 
+    default.
     """
+    URL='http://localhost.com/'
+
+    my_url = Url(f"{URL}")
+    my_url.run()
+
     expected_file_name = f'0/localhost_com.html'
 
     assert os.path.isdir('0')
-
+    assert os.path.isfile(expected_file_name)
+    # assert os.path.isfile(expected_file_nae)
 
 # def file_exist(
